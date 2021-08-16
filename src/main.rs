d60aa0795eb9240f0655f1aea1537bff118ea8d4
Tony X <zylphrex@gmail.com>
2021-08-16 13:22:14 -0400

    handle errors in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fmt;
+
 use structopt::StructOpt;
 use thirtyfour_sync;
 use tokio;
@@ -36,67 +39,70 @@ struct Cli {
     enbridge_password: Option<String>,
 }
 
+#[derive(Debug)]
+enum OkaneError {
+    WebDriverError(thirtyfour_sync::error::WebDriverError),
+    TwilioError(twilio::TwilioError),
+}
+
+impl fmt::Display for OkaneError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", 0)
+    }
+}
+
+impl Error for OkaneError {}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), OkaneError> {
     let args = Cli::from_args();
 
     let mut caps = thirtyfour_sync::DesiredCapabilities::chrome();
     if args.headless {
-        caps.set_headless().ok();
+        caps.set_headless().map_err(|err| OkaneError::WebDriverError(err))?;
     }
 
-    match driver::OkaneDriver::new(&args.server, &caps) {
-        Ok(driver) => {
-            let mut balances = vec!["💸 Unpaid bills".to_string()];
+    let driver = driver::OkaneDriver::new(&args.server, &caps).map_err(|err| OkaneError::WebDriverError(err))?;
 
-            if args.rogers {
-                let username = &args.rogers_username.unwrap();
-                let password = &args.rogers_password.unwrap();
+    let mut balances = vec!["💸 Unpaid bills".to_string()];
 
-                let rogers_scraper = scraper::rogers::RogersScraper::new(&driver, username, password);
-                match rogers_scraper.run() {
-                    Ok(balance) => {
-                        println!("rogers balance {:?}", balance);
-                        if balance > 0.0 {
-                            balances.push(format!("- Rogers: ${:.2}", balance));
-                        }
-                    },
-                    _ => (),
-                }
-            }
+    if args.rogers {
+        let username = &args.rogers_username.unwrap();
+        let password = &args.rogers_password.unwrap();
 
-            if args.enbridge {
-                let username = &args.enbridge_username.unwrap();
-                let password = &args.enbridge_password.unwrap();
-
-                let enbridge_scraper = scraper::enbridge::EnbridgeScraper::new(&driver, username, password);
-                match enbridge_scraper.run() {
-                    Ok(balance) => {
-                        println!("enbridge balance {:?}", balance);
-                        if balance > 0.0 {
-                            balances.push(format!("- Enbridge: ${:.2}", balance));
-                        }
-                    },
-                    _ => (),
-                }
-            }
-
-            if balances.len() > 1 && args.callee.len() > 0 {
-                let sender = &args.caller;
-                let receiver = &args.callee;
-                let account_id = &args.twilio_account_id;
-                let auth_token = &args.twilio_auth_token;
-                let twilio_client = twilio::Client::new(account_id, auth_token);
-
-                let message = balances.join("\n");
-
-                println!("message: {} sent from {} to {}", message, sender, receiver);
-                let envelope = twilio::OutboundMessage::new(sender, receiver, &message);
-                twilio_client.send_message(envelope).await.ok();
-            }
-
-            driver.quit().ok();
-        },
-        _ => (),
+        let rogers_scraper = scraper::rogers::RogersScraper::new(&driver, username, password);
+        let balance = rogers_scraper.run().map_err(|err| OkaneError::WebDriverError(err))?;
+        println!("rogers balance {:?}", balance);
+        if balance > 0.0 {
+            balances.push(format!("- Rogers: ${:.2}", balance));
+        }
     }
+
+    if args.enbridge {
+        let username = &args.enbridge_username.unwrap();
+        let password = &args.enbridge_password.unwrap();
+
+        let enbridge_scraper = scraper::enbridge::EnbridgeScraper::new(&driver, username, password);
+        let balance = enbridge_scraper.run().map_err(|err| OkaneError::WebDriverError(err))?;
+        println!("enbridge balance {:?}", balance);
+        if balance > 0.0 {
+            balances.push(format!("- Enbridge: ${:.2}", balance));
+        }
+    }
+
+    if balances.len() > 1 && args.callee.len() > 0 {
+        let sender = &args.caller;
+        let receiver = &args.callee;
+        let account_id = &args.twilio_account_id;
+        let auth_token = &args.twilio_auth_token;
+        let twilio_client = twilio::Client::new(account_id, auth_token);
+
+        let message = balances.join("\n");
+
+        println!("message: {} sent from {} to {}", message, sender, receiver);
+        let envelope = twilio::OutboundMessage::new(sender, receiver, &message);
+        twilio_client.send_message(envelope).await.map_err(|err| OkaneError::TwilioError(err))?;
+    }
+
+    driver.quit().map_err(|err| OkaneError::WebDriverError(err))
 }
